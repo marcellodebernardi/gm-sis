8f5ca91e7c7b1ac50986a36b871613eaec7207c5
Ebube Abara <ebubeabara3@outlook.com>
2017-01-19 20:36:18 +0000

    Adding <Customer account> package

---
+++ Plookify/src/customer/ComponentLoader.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package customer;
+
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author EBUBECHUKWU
+ */
+public class ComponentLoader {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "The application works.");
+    }
+}
