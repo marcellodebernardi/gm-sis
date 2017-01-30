982d669869d760cfc6b964b4c833afe155ff4275
Dillon Vaghela <d.vaghela@se15.qmul.ac.uk>
2017-01-30 16:14:18 +0000

    Changing folder names and updated work

--- master/working/src/main/Main.java
+++ master/working/src/main/Main.java
@@ -2,6 +2,10 @@ package main;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 /**
  * @author Marcello De Bernardi
@@ -10,8 +14,17 @@ import javafx.stage.Stage;
  */
 public class Main extends Application {
 
+    public static void main (String[] args)
+    {
+        launch(args);
+    }
     @Override
     public void start(Stage stage) {
+
+        Parent root = FXMLLoader.load(getClass().getResource("test.fxml"));
+        primaryStage.setTitle("Hello");
+        primaryStage.setScene(new Scene(root, 300, 300));
+        primaryStage.show();
 
     }
 }
