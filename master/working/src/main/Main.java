72e68057cdbb2543473b8961d78011f5a042a5a8
Dillon Vaghela <d.vaghela@se15.qmul.ac.uk>
2017-01-30 16:47:04 +0000

    login update

--- master/working/src/main/Main.java
+++ master/working/src/main/Main.java
@@ -4,8 +4,9 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import javafx.scene.Scene;
 
 /**
  * @author Marcello De Bernardi
@@ -16,14 +17,13 @@ public class Main extends Application {
 
     public static void main (String[] args)
     {
-        launch(args);
+        Application.launch(args);
     }
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage primaryStage) throws Exception {
 
-        Parent root = FXMLLoader.load(getClass().getResource("test.fxml"));
-        primaryStage.setTitle("Hello");
-        primaryStage.setScene(new Scene(root, 300, 300));
+        Pane mainPane = (Pane) FXMLLoader.load(getClass().getResource("/resources/test.fxml"));
+        primaryStage.setScene(new Scene(mainPane));
         primaryStage.show();
 
     }
