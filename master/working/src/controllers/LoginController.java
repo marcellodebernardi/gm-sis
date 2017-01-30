c4a70b5023915e75fa066257da3c4717e1af2fe6
Dillon Vaghela <d.vaghela@se15.qmul.ac.uk>
2017-01-30 15:58:35 +0000

    Changing folder names and updated work

---
+++ master/working/src/controllers/LoginController.java
@@ -0,0 +1,13 @@
+package controllers;
+
+import javafx.scene.control.Button;
+
+public class LoginController {
+
+    public Button loginBtn;
+
+    public void handleButtonClick()
+    {
+        System.out.println("working");
+    }
+}
