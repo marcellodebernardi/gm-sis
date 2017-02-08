d34ec0317d757a8f5e3a17c21ce72d06025d7c61
Ebube Abara <ebubeabara3@outlook.com>
2017-02-08 22:28:51 +0000

    Created Customer.java and CustomerType.java classes. Customer has entities of customer and CustomerType has the customer type entities. First version

---
+++ master/working/src/entities/Customer.java
@@ -0,0 +1,127 @@
+package entities;
+import logic.Criterion;
+
+/**
+ * Created by EBUBECHUKWU on 08/02/2017.
+ */
+
+public class Customer implements Criterion
+{
+    private int customerID;
+    private String customerSurname;
+    private String customerFirstname;
+    private String customerAddress;
+    private String customerPostcode;
+    private String customerPhone;
+    private String customerEmail;
+    private CustomerType customerType;
+
+    public Customer(int cID, String cSurname, String cFirstname, String cAddress, String cPostcode, String cPhone, String cEmail, CustomerType cType)
+    {
+        customerID = cID;
+        customerSurname = cSurname;
+        customerFirstname = cFirstname;
+        customerAddress = cAddress;
+        customerPostcode = cPostcode;
+        customerPhone = cPhone;
+        customerEmail = cEmail;
+        customerType = cType;
+    }
+
+    //gets unique identifier for customer
+    public int getCustomerID()
+    {
+        return customerID;
+    }
+
+    //sets unique identifier for customer
+    public void setCustomerID(int custID)
+    {
+        customerID = custID;
+    }
+
+    //gets customer's surname
+    public String getCustomerSurname()
+    {
+        return customerSurname;
+    }
+
+    //sets customer's surname
+    public void setCustomerSurname(String custSurname)
+    {
+        customerSurname = custSurname;
+    }
+
+    //gets customer's first name
+    public String getCustomerFirstname()
+    {
+        return customerFirstname;
+    }
+
+    //sets customer's first name
+    public void setCustomerFirstname(String custFirstname)
+    {
+        customerFirstname = custFirstname;
+    }
+
+    //gets customer's address
+    public String getCustomerAddress()
+    {
+        return customerAddress;
+    }
+
+    //sets customer's address
+    public void setCustomerAddress(String custAddress)
+    {
+        customerAddress = custAddress;
+    }
+
+    //gets customer's postcode
+    public String getCustomerPostcode()
+    {
+        return customerPostcode;
+    }
+
+    //sets customer's postcode
+    public void setCustomerPostcode(String custPostcode)
+    {
+        customerPostcode = custPostcode;
+    }
+
+    //gets customer's phone number
+    public String getCustomerPhone()
+    {
+        return customerPhone;
+    }
+
+    //sets customer's phone number
+    public void setCustomerPhone(String custPhone)
+    {
+        customerPhone = custPhone;
+    }
+
+    //gets customer's email address
+    public String getCustomerEmail()
+    {
+        return customerEmail;
+    }
+
+    //sets customer's email address
+    public void setCustomerEmail(String custEmail)
+    {
+        customerEmail = custEmail;
+    }
+
+    //gets customer type (private or business)
+    public CustomerType getCustomerType()
+    {
+        return customerType;
+    }
+
+    //sets customer type (private or business)
+    public void setCustomerType(CustomerType custType)
+    {
+        customerType = custType;
+    }
+
+}
---
+++ master/working/src/entities/CustomerType.java
@@ -0,0 +1,16 @@
+package entities;
+import logic.Criterion;
+/**
+ * Created by EBUBECHUKWU on 08/02/2017.
+ */
+public class CustomerType implements Criterion
+{
+    private String privateIndividual = "Private Individual";
+    private String businessIndividual = "Business Individual";
+
+    //public CustomerType()
+    //{}
+
+
+
+}
