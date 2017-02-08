b47fa4694ab30470fad0cc3f148a2f4d9461490d
ec15496 <murad.ahmed@se15.qmul.ac.uk>
2017-02-08 16:17:25 +0000

    GUI
    GUI for my module. All subject to change.
    
    Murad

---
+++ master/working/src/logic/SpecRepairSystem.java
@@ -0,0 +1,39 @@
+package logic;
+
+
+import entities.SpecialistRepairCenter;
+import java.util.List;
+import java.util.ArrayList;
+/**
+ * @author muradahmed
+ */
+public class SpecRepairSystem {
+
+    private static SpecRepairSystem instance;
+    private CriterionRepository persistence;
+
+
+    private SpecRepairSystem(CriterionRepository persistence) {
+        this.persistence = persistence;
+    }
+
+
+    /**
+     * Returns the singleton instance of the SpecRepairSystem.
+     *
+     * @return singleton instance of SpecRepairSystem
+     */
+    public SpecRepairSystem getInstance(CriterionRepository persistence) {
+        if (instance == null) instance = new SpecRepairSystem(persistence);
+        return instance;
+    }
+
+    /**
+     * Returns a list of Specialist Repair Centers
+     * @return list of Specialist Repair Centers
+     */
+    public List<SpecialistRepairCenter>getRepairCenterList(){
+        ArrayList<SpecialistRepairCenter> arrayList = new ArrayList<SpecialistRepairCenter>();
+        return null;
+    }
+}
