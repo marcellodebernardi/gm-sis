8204df1f8f2dfac18ccc8ec4c07caad1a6e433a3
Dillon Vaghela <d.vaghela@se15.qmul.ac.uk>
2017-02-09 22:36:06 +0000

    updated VehicleSys

--- master/working/src/logic/VehicleSys.java
+++ master/working/src/logic/VehicleSys.java
@@ -1,7 +1,58 @@
 package logic;
 
+import entities.Vehicle;
+import persistence.DatabaseRepository;
+import java.util.*;
+
 /**
  * Created by DillonVaghela on 2/9/17.
  */
 public class VehicleSys {
+
+    private static VehicleSys instance;
+    private CriterionRepository persistence = DatabaseRepository.getInstance();
+
+    private VehicleSys(CriterionRepository persistence) {
+        this.persistence = persistence;
+
+    }
+
+    public List<Vehicle> getVehiclesList(){
+        ArrayList<Vehicle> arrayList = new ArrayList<Vehicle>();
+        return arrayList;
+    }
+
+    public List<Vehicle> searchVehicle() {
+
+        /** TODO: create search method
+         *
+         */
+
+        List<Vehicle> vehicleList = new ArrayList<Vehicle>();
+        return vehicleList;
+
+    }
+
+    public boolean addVehicle(){
+
+        return false;
+
+    }
+
+    public boolean deleteVehicle(){
+
+        return false;
+    }
+
+    public boolean editVehicle(){
+
+        return false;
+
+    }
+
+    public VehicleSys getInstance(CriterionRepository persistence) {
+        if (instance == null) instance = new VehicleSys(persistence);
+        return instance;
+    }
+
 }
