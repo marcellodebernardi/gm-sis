43d9d44adb106c23eb7d31b271a2979a53b00bf5
ec15265 <m.e.debernardi@se15.qmul.ac.uk>
2017-03-18 00:29:40 +0000

    Set retention policy for annotations

--- master/working/src/domain/Lazy.java
+++ master/working/src/domain/Lazy.java
@@ -1,10 +1,14 @@
 package domain;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 /**
  *@author Marcello De Bernardi
  *
  * Annotation with no functional significance. Denotes methods that effectively implement
  * lazy properties on objects.
  */
+@Retention(RetentionPolicy.SOURCE)
 @interface Lazy {
 }
